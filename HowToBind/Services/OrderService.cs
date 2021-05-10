6db20b5162efb74a0459ed8722f218820d4a6d9b
Christopher Edwards <windseed@gmail.com>
2021-05-10 13:10:31 +0100

    MrC aka Shaun Curtis

---
+++ HowToBind/Services/OrderService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HowToBind.Services
+{
+    public class OrderService
+    {
+        public string ShippingAddress
+        {
+            get => _ShippingAddress;
+            set
+            {
+                if (!_ShippingAddress.Equals(value))
+                {
+                    NotifyOrderChanged();
+                    _ShippingAddress = value;
+                }
+            }
+        }
+        private string _ShippingAddress = string.Empty;
+
+
+        public string BillingAddress
+        {
+            get => _BillingAddress;
+            set
+            {
+                if (!_BillingAddress.Equals(value))
+                {
+                    NotifyOrderChanged();
+                    _BillingAddress = value;
+                }
+            }
+        }
+        private string _BillingAddress = string.Empty;
+
+        public event EventHandler OrderChanged;
+
+        public void NotifyOrderChanged()
+            => OrderChanged.Invoke(this, EventArgs.Empty);
+    }
+}
--- HowToBind/Startup.cs
+++ HowToBind/Startup.cs
@@ -1,4 +1,5 @@
 using HowToBind.Data;
+using HowToBind.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -29,6 +30,7 @@ namespace HowToBind
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddScoped<OrderService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
