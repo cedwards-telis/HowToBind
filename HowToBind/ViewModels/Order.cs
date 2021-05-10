ed5c175cb200f13e863248007d8a340428431fb2
Christopher Edwards <windseed@gmail.com>
2021-05-10 17:21:04 +0100

    CascadingParameter Working-ish

---
+++ HowToBind/ViewModels/Order.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HowToBind.ViewModels
+{
+    public class Order
+    {
+        string billingAddressText;
+        public string BillingAddressText
+        {
+            get => billingAddressText;
+            set
+            {
+                billingAddressText = value;
+                if (sameAsBillingAddress)
+                    shippingAddressText = billingAddressText;
+                NotifyOrderChanged();
+            }
+        }
+        string shippingAddressText;
+        public string ShippingAddressText
+        {
+            get => shippingAddressText;
+            set
+            {
+                shippingAddressText = value;
+                NotifyOrderChanged();
+            }
+        }
+        bool sameAsBillingAddress;
+        public bool SameAsBillingAddress
+        {
+            get => sameAsBillingAddress;
+            set
+            {
+                sameAsBillingAddress = value;
+                if (sameAsBillingAddress)
+                    shippingAddressText = billingAddressText;
+                NotifyOrderChanged();
+            }
+        }
+
+        public event EventHandler OrderChanged;
+        public void NotifyOrderChanged()
+            => OrderChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
