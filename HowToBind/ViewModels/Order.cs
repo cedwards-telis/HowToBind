fd581c00c0228ff19d9820707c12d52dbd6aa289
Christopher Edwards <windseed@gmail.com>
2021-05-11 11:13:01 +0100

    Doesn't work.  Simplify.
    Member 'HowToBind.ViewModels.Order/BillingAddress <CurrentBillingAddress>k__BackingField' is declared in another module

---
+++ HowToBind/ViewModels/Order.cs
@@ -0,0 +1,31 @@
+﻿using Cortex.Net.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HowToBind.ViewModels
+{
+    [Observable]
+    public class Order
+    {
+        public BillingAddress CurrentBillingAddress { get; set; } = new();
+        [Observable]
+        public class BillingAddress
+        {
+            public string BillingAddressText { get; set; }
+        }
+
+        public ShippingAddress CurrentShippingAddress { get; set; } = new();
+        [Observable]
+        public class ShippingAddress
+        {
+            public string ShippingAddressText { get; set; }
+            public bool SameAsBillingAddress { get; set; }
+        }
+
+        [Computed]
+        public string ComputedShippingAddressText => CurrentShippingAddress.SameAsBillingAddress ? 
+            CurrentBillingAddress.BillingAddressText : CurrentShippingAddress.ShippingAddressText;
+    }
+}
